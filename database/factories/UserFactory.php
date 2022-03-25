a1f8793de5530b1023745a861ceeefbe262bc4f0
tinsaemesfin <qmesfin@gmail.com>
2022-03-25 11:55:54 +0300

    first commit end of ep-2

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Tenant;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'role' => 'admin',
+            'email_verified_at' => now(),
+            'password' => bcrypt('12345678'), // password
+            'remember_token' => Str::random(10),
+            'tenant_id'=> Tenant::factory()->create()->id,
+        ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     *
+     * @return static
+     */
+    public function unverified()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'email_verified_at' => null,
+            ];
+        });
+    }
+}
